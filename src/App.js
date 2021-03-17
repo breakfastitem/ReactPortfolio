932b19bcbbf558b71b0f78d546318ae6dcd3f756
breakfastitem <andrew.ehrman@gmail.com>
2021-03-17 00:25:14 -0400

    install react-router-dom and set up switch so navigation will work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from './components/Header/Header'
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={() => <Header />} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
